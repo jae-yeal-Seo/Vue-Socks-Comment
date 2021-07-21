080150762bc7d64a67bcd7317aa88d0445280c4f
woduf9173@naver.com <woduf9173@naver.com>
2021-07-21 19:01:33 +0900

    양말+댓글

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductPage></ProductPage>
+    <Comment></Comment>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductPage from "@/components/ProductPage.vue";
+import Comment from "@/components/Comment.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ProductPage,
+    Comment,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/css/styles.css";
 </style>
---
+++ src/components/Comment.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input type="text" v-model="user" />
+    <input type="text" v-model="comment" />
+    <button @click="save">Save</button><br />
+    <ul>
+      <li v-for="msg in message" :key="msg.comment">
+        <p>{{ msg.user }}:{{ msg.comment }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: null,
+      comment: null,
+      message: [],
+    };
+  },
+  methods: {
+    save() {
+      const message = {
+        user: this.user,
+        comment: this.comment,
+      };
+      //얜 객체구나.(message)
+
+      if (message.user != null || message.comment != null) {
+        this.message.unshift(message);
+        this.user = null;
+        this.comment = null;
+        //데이터를 바꾸는게 v-model의 애들도 바꾸는 것이다.
+      }
+    },
+  },
+};
+</script>
